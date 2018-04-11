bdea61ba773e930247a75d9c3cea44ec3f3e1f03
Christian Zumbrunnen <chzumbrunnen@chruezundquer.ch>
2018-04-11 10:50:38 +0200

    Initial Commit (existing theme without changes)

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'avada-parent-stylesheet', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+function avada_lang_setup() {
+	$lang = get_stylesheet_directory() . '/languages';
+	load_child_theme_textdomain( 'Avada', $lang );
+}
+add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
