69b5fdefad19f04193f607f616c95bb1b190fc7e
Christian Zumbrunnen <chzumbrunnen@chruezundquer.ch>
2018-04-11 11:02:20 +0200

    Custom Fields Output

--- functions.php
+++ functions.php
@@ -14,3 +14,24 @@ add_action( 'after_setup_theme', 'avada_lang_setup' );
 
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+
+// this filter adds the custom field values to the json output
+add_filter( 'tribe_rest_event_data', 'sp_add_event_customfields' );
+  
+function sp_add_event_customfields($data) {
+$event_id = $data['id'];
+$data = array_merge( $data, 
+
+array("custom_fields" => array(
+    'price'  => get_post_meta( $event_id, '_ecp_custom_2', true ),
+    'price2'  => get_post_meta( $event_id, '_ecp_custom_3', true ),
+
+    'Referent' => get_post_meta( $event_id, '_ecp_custom_3', true ),
+    'Details_Anmeldung' => get_post_meta( $event_id, '_ecp_custom_4', true ),
+    'Fotorechte' => get_post_meta( $event_id, '_ecp_custom_7', true ),
+    'Preis' => get_post_meta( $event_id, '_ecp_custom_16', true ),
+
+    )    ));
+   
+    return $data;
+}
